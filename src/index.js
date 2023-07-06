931ad30130256dc2ad7a4a39783c09ca19f9515b
Your Name <you@example.com>
2023-07-06 10:57:50 +0530

    react-app

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { sendToVercelAnalytics } from './vitals';
+import Game from './Game';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Game />
   </React.StrictMode>,
   document.getElementById('root')
 );
